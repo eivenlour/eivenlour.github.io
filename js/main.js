34bee5be563562c91286373955394d10ec331b28
Eivenlour David <eivenlou@ualberta.ca>
2019-08-05 13:40:31 -0600

    new logo

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 /* Preloader */
 
 $(window).load(function() {
-    $(".preloader").delay(2700).fadeOut("slow");
-    $(".overlayer").delay(4000).fadeOut("slow");
+    $(".preloader").delay(5000).fadeOut("slow");
+    $(".overlayer").delay(5500).fadeOut("slow");
 
 })
