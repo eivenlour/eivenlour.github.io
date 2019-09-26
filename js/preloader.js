fd07db8e69f8c61a748d9268ec06dffe5ea26767
Eivenlour David <eivenlou@ualberta.ca>
2019-09-26 12:38:34 -0600

    last fix for this mobile preloader bug

--- js/preloader.js
+++ js/preloader.js
@@ -1,13 +1,21 @@
 /* Preloader */
 
+/* Disable scrolling */
+function noScroll() {
+    window.scrollTo(0, 0);
+}
+
 $(window).on('load', function() {
     $(".main-content").css('visibility', 'hidden');
-    $("body").css('overflow', 'hidden');  
+    $("body").css('overflow', 'hidden');
+    $(window).on('scroll', noScroll);
     $(".preloader").delay(5000).fadeOut("slow", function () {
         $(".main-content").css('visibility', 'visible');
         $("body").css('overflow', 'auto');  
     });
-    $(".overlayer").delay(5500).fadeOut("slow");
+    $(".overlayer").delay(5500).fadeOut("slow", function(){
+        $(window).off('scroll', noScroll);
+    });
 });
 
   
