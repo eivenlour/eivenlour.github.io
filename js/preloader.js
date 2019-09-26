4b84801e1116e3c2a0e516e972dc34ac11c1ce1d
Eivenlour David <eivenlou@ualberta.ca>
2019-09-26 11:41:44 -0600

    mobile preloader issue fixedgit status

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,18 @@
 /* Preloader */
-$(window).load(function() {
-    $(".preloader").delay(5000).fadeOut("slow");
-    $(".overlayer").delay(5500).fadeOut("slow");
 
+/* Disable scrolling */
+function noScroll() {
+    window.scrollTo(0, 0);
+}
+
+$(window).load(function() {
+    $(window).on('scroll', noScroll);
+    $(".preloader").delay(5000).fadeOut("slow");
+    $(".overlayer").delay(5500).fadeOut("slow", function() {
+        $(window).delay(5500).off('scroll', noScroll);
+    });
 });
+
+  
+
 
