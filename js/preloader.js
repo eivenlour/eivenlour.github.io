43917eaf9516f694b840808efcdb8eb9a28bb965
Eivenlour David <eivenlou@ualberta.ca>
2019-09-26 12:32:01 -0600

    minor fix

--- js/preloader.js
+++ js/preloader.js
@@ -1,15 +1,13 @@
 /* Preloader */
 
-/* Disable scrolling */
-function noScroll() {
-    window.scrollTo(0, 0);
-}
-
 $(window).on('load', function() {
-    $(".preloader").delay(5000).fadeOut(2000, function () {
+    $(".main-content").css('visibility', 'hidden');
+    $("body").css('overflow', 'hidden');  
+    $(".preloader").delay(5000).fadeOut("slow", function () {
         $(".main-content").css('visibility', 'visible');
         $("body").css('overflow', 'auto');  
     });
+    $(".overlayer").delay(5500).fadeOut("slow");
 });
 
   
