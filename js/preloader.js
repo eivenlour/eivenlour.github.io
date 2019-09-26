18e1dd16897a644bda8998221638371bcf22505a
Eivenlour David <eivenlou@ualberta.ca>
2019-09-26 11:57:37 -0600

    minor fix

--- js/preloader.js
+++ js/preloader.js
@@ -5,7 +5,7 @@ function noScroll() {
     window.scrollTo(0, 0);
 }
 
-$(window).load(function() {
+$(document).ready(function() {
     $(window).on('scroll', noScroll);
     $(".preloader").delay(5000).fadeOut("slow");
     $(".overlayer").delay(5500).fadeOut("slow", function() {
